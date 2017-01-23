53a941141b747a0e5652e645a4ff242f4c1364ae
Rafael Takahashi <rafael.k.takahashi@gmail.com>
2017-01-23 12:35:40 -0200

    Moved parser class implementation to Remaps.cpp

--- MultiKeys/MultiKeys/Remaps.cpp
+++ MultiKeys/MultiKeys/Remaps.cpp
@@ -39,7 +39,9 @@ BOOL Multikeys::Remapper::LoadSettings(std::string filename)		// parser
 	if (!file.is_open())
 		return FALSE;
 
-	return Parser::ReadFile(&file, &keyboards);
+	BOOL result = Parser::ReadFile(&file, &keyboards);
+	file.close();
+	return result;
 }
 BOOL Multikeys::Remapper::LoadSettings(std::wstring filename)
 {
@@ -48,15 +50,11 @@ BOOL Multikeys::Remapper::LoadSettings(std::wstring filename)
 	if (!file.is_open())
 		return FALSE;
 
-	return Parser::ReadFile(&file, &keyboards);
+	BOOL result = Parser::ReadFile(&file, &keyboards);
+	file.close();
+	return result;
 }
 
-
-// Temporary function to test the parser
-void ParserTest()
-{
-	return;
-}
 
 
 
@@ -125,3 +123,299 @@ BOOL Multikeys::Remapper::SimulateKeystroke(KEYSTROKE_OUTPUT key)
 
 
 
+
+
+/*----Parser static class----*/
+
+BOOL Multikeys::Parser::ReadSymbol(std::ifstream* stream, std::string* symbol)
+{
+	int input_char = 0;		// buffer for one character
+	symbol->clear();
+
+	if (stream->peek() == -1)			// If the very next thing is the end
+		return FALSE;					// then return false
+
+	while (isalpha(stream->peek()))			// if the next character is alphabetic
+	{
+		(*symbol) += (char)stream->get();		// then add that character to the string
+		if (stream->peek() == -1)				// if the next thing is an end-of-file,
+			return TRUE;						// then we're done
+	}											// and repeat
+
+												// reaching this point means we've encountered a non-alphabetic character
+	return TRUE;
+}
+
+
+BOOL Multikeys::Parser::ReadKeyboardName(std::ifstream* stream, KEYBOARD* kb)
+{
+	// from <codecvt>, the object that will convert between narrow and wide strings (C++11 and newer)
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	// Because we read in UTF-8, but device names are stored internally as WCHARs.
+
+	auto buffer = std::string();					// utf-8
+	auto wideBuffer = std::wstring();				// utf-16
+
+	while (!isspace(stream->peek()) && stream->peek() != ')')
+	{
+		buffer += stream->get();
+	}
+	if (buffer == "") return FALSE;		// kb name wasn't there
+	wideBuffer = converter.from_bytes(buffer);			// convert and then store it in the wide buffer
+	swprintf_s(kb->device_name, kb->device_name_sizeof, L"%ls", wideBuffer.c_str());
+	return TRUE;
+}
+
+
+BOOL Multikeys::Parser::ReadModifierHex(std::ifstream* stream, BYTE* modifiers, UINT32* code)
+{
+	int input_char = 0;		// buffer for one character
+
+	int sideModifier = 0;	// 0: next modifier applies to left and right
+							// 1: left modifier
+							// 2: right modifier
+
+	(*modifiers) = 0;		// clearing...
+	(*code) = 0;
+
+	input_char = stream->peek();		// evaluate *next* character
+	while (true)
+	{
+		switch (input_char)					// TODO: Put this switch into a loop
+		{
+		case '<':					// next modifier is left only
+			sideModifier = 1;
+			stream->get();
+			break;
+
+		case '>':					// next modifier is right only
+			sideModifier = 2;
+			stream->get();
+			break;
+
+		case '#':					// Winkey (unaffected by side modifiers)
+			if (sideModifier == 1 || sideModifier == 0)
+				*modifiers |= MODIFIER_LWIN;
+			if (sideModifier == 2 || sideModifier == 0)
+				*modifiers |= MODIFIER_RWIN;		// (I've never seen a right Winkey)
+			sideModifier = 0;
+			stream->get();
+			break;
+
+		case '^':					// Ctrl
+			if (sideModifier == 1 || sideModifier == 0)
+				*modifiers |= MODIFIER_LCTRL;
+			if (sideModifier == 2 || sideModifier == 0)
+				*modifiers |= MODIFIER_RCTRL;
+			sideModifier = 0;
+			stream->get();
+			break;
+
+		case '!':					// Alt
+			if (sideModifier == 1 || sideModifier == 0)
+				*modifiers |= MODIFIER_LALT;
+			if (sideModifier == 2 || sideModifier == 0)
+				*modifiers |= MODIFIER_RALT;
+			sideModifier = 0;
+			stream->get();
+			break;
+
+		case '+':					// Shift
+			if (sideModifier == 1 || sideModifier == 0)
+				*modifiers |= MODIFIER_LSHIFT;
+			if (sideModifier == 2 || sideModifier == 0)
+				*modifiers |= MODIFIER_RSHIFT;
+			sideModifier = 0;
+			stream->get();
+			break;
+
+		case '0':
+		{	// brackets for locality
+			// expect an 'x' or 'X'
+			stream->get();
+			input_char = stream->peek();
+			if (input_char != 'x' && input_char != 'X')
+				return FALSE;
+			stream->get();
+			// while the next character is a hexadecimal digit, read it
+			std::string numberBuffer = std::string();
+			while (isxdigit(stream->peek()))
+			{
+				numberBuffer += stream->get();
+			}
+			if (sscanf_s(numberBuffer.c_str(), "%x", code) != 1)
+				return FALSE;		// that's one way to cast
+									// we're not trying to be fast, exactly
+									// scanf returns the amount of variables scanned
+			else return TRUE;
+		}
+
+		default:			// Do not extract character
+			return FALSE;
+		}	// end of switch
+	}	// loop
+}
+
+
+BOOL Multikeys::Parser::ReadInputKeystroke(std::ifstream* stream, KEYSTROKE_INPUT* trigger)
+{
+	UINT32 code = 0;
+	BOOL result = ReadModifierHex(stream, &(trigger->modifiers), &code);
+	trigger->scancode = (USHORT)code;			// lossy cast, but shouldn't lose any data
+	return result;
+}
+
+
+BOOL Multikeys::Parser::ReadVirtualkey(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke)
+{
+	keystroke->flags = 0;
+	return ReadModifierHex(stream, &(keystroke->modifiers), &(keystroke->codepoint));
+}
+
+
+BOOL Multikeys::Parser::ReadUnicode(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke)
+{
+	keystroke->flags = KEYEVENTF_UNICODE;
+
+	// All modifiers will be read but ignored
+	while (stream->peek() == '<'
+		|| stream->peek() == '>'
+		|| stream->peek() == '#'
+		|| stream->peek() == '^'
+		|| stream->peek() == '!'
+		|| stream->peek() == '+')
+		stream->get();
+
+	if (stream->peek() != '0')			// expect a hexadecimal
+		return FALSE;
+	stream->get();
+	if (stream->peek() != 'x' && stream->peek() != 'X')
+		return FALSE;
+	stream->get();
+
+	std::string numberBuffer = std::string();
+	while (isxdigit(stream->peek()))		// read all following hex digits
+	{
+		numberBuffer += stream->get();
+	}
+	if (sscanf_s(numberBuffer.c_str(), "%x", &(keystroke->codepoint)) != 1)
+		return FALSE;
+
+	return TRUE;
+}
+
+
+BOOL Multikeys::Parser::ReadFile(std::ifstream* stream, std::vector<KEYBOARD> * ptrVectorKeyboard)
+{
+	// having a function explicitly expect such a specific map is not very nice, but that's what I could do.
+
+	// Also, in the future we should consider supporting UTF-16 filenames. I don't know how to do that.
+
+	setlocale(LC_ALL, "");						// Set the locale, just in case
+
+
+	ptrVectorKeyboard->clear();					// clear all keyboards with the maps in them
+
+
+												// hold one line
+	auto lineBuffer = std::string();
+
+	// hold one symbol
+	auto symbol = std::string();
+
+	// hold the current keyboard being written to
+	auto keyboard = KEYBOARD();
+
+	// input and output
+	auto input = KEYSTROKE_INPUT();
+	auto output = KEYSTROKE_OUTPUT();
+
+	// one character
+	int read_char = 0;
+
+
+	// First term should be a "keyboard"
+	if (!ReadSymbol(stream, &symbol)) return FALSE;
+	if (symbol != "keyboard") return FALSE;
+	// expect left parenthesis
+	read_char = stream->get();
+	if (read_char != '(') return FALSE;
+	// read keyboard name
+	if (!ReadKeyboardName(stream, &keyboard)) return FALSE;
+	// expect right parenthesis
+	read_char = stream->get();
+	if (read_char != ')') return FALSE;
+	// jump to next line
+	getline(*stream, lineBuffer);
+	// begin loop
+	while (true)
+	{
+		if (!ReadSymbol(stream, &symbol))
+		{
+			// save keyboard and close
+			ptrVectorKeyboard->push_back(keyboard);
+			return TRUE;
+		}
+		// evaluate symbol
+		if (symbol == "unicode")
+		{
+			// case 1 - unicode
+			// expect a left parenthesis
+			read_char = stream->get();
+			if (read_char != '(') return FALSE;
+			// first parameter: an input keystroke
+			if (!ReadInputKeystroke(stream, &input)) return FALSE;
+			// expect a comma
+			read_char = stream->get();
+			if (read_char != ',') return FALSE;
+			// second parameter: an output keystroke with a unicode codepoint
+			if (!ReadUnicode(stream, &output)) return FALSE;
+			// expect a right parenthesis
+			read_char = stream->get();
+			if (read_char != ')') return FALSE;
+			// place input and output into current keyboard
+			keyboard.remaps.insert((std::pair<KEYSTROKE_INPUT, KEYSTROKE_OUTPUT>(input, output)));
+		}
+		else if (symbol == "virtual")
+		{
+			// case 2 - virtual key
+			// expect a left parenthesis
+			read_char = stream->get();
+			if (read_char != '(') return FALSE;
+			// first parameter: an input keystroke
+			if (!ReadInputKeystroke(stream, &input)) return FALSE;
+			// expect a comma
+			read_char = stream->get();
+			if (read_char != ')') return FALSE;
+			// second parameter: an output keystroke with a virtual key
+			if (!ReadVirtualkey(stream, &output)) return FALSE;
+			// expect a right parenthesis
+			read_char = stream->get();
+			if (read_char != ')') return FALSE;
+			// place input and output into current keyboard
+			keyboard.remaps.insert((std::pair<KEYSTROKE_INPUT, KEYSTROKE_OUTPUT>(input, output)));
+		}
+		else if (symbol == "keyboard")
+		{
+			// case 3 - new keyboard
+			// save this keyboard
+			ptrVectorKeyboard->push_back(keyboard);
+			// read a new one=
+			if (!ReadSymbol(stream, &symbol)) return FALSE;
+			if (symbol != "keyboard") return FALSE;
+			// expect left parenthesis
+			read_char = stream->get();
+			if (read_char != '(') return FALSE;
+			// read keyboard name
+			if (!ReadKeyboardName(stream, &keyboard)) return FALSE;
+			// expect right parenthesis
+			read_char = stream->get();
+			if (read_char != ')') return FALSE;
+			// jump to next line
+			getline(*stream, lineBuffer);
+		}
+		// jump line
+		getline(*stream, lineBuffer);
+
+	}	// loop
+}
--- MultiKeys/MultiKeys/Remaps.h
+++ MultiKeys/MultiKeys/Remaps.h
@@ -89,332 +89,64 @@ namespace Multikeys
 
 	};
 
+
+
+
+
+
+	// LoadSettings will use this class (only one of its methods, actually
+	// (making a static class instead of a namespace so that a bunch of its
+	// methods can be private and out of the way)
+	class Parser
+	{
+	private:
+		Parser() {}	// Nobody can make an instance of this.
+
+
+					// Reads alphabetic characters until a non-alphanumeric character is found
+					// The non-alpha is not read and not extracted
+					// Result is stored in the symbol parameter
+					// return value of FALSE means no symbol was found (end-of-file was found instead)
+		static BOOL ReadSymbol(std::ifstream* stream, std::string* symbol);
+		
+
+
+
+		// Read a keyboard name, such as \\?\ACPI#PNP0303#4&231b8599&0#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}
+		// and store it in kb's device_name field
+		// Will only stop at whitespace (including newline) or a right parenthesis
+		static BOOL ReadKeyboardName(std::ifstream* stream, KEYBOARD* kb);
+
+
+		static BOOL ReadModifierHex(std::ifstream* stream, BYTE* modifiers, UINT32* code);
+		
+
+
+		// Read an input keystroke, such as <+<!0x47
+		// (modifiers and a scancode)
+		// and place it into a trigger structure
+		// Returns FALSE upon encountering an unrecognized character
+		static BOOL ReadInputKeystroke(std::ifstream* stream, KEYSTROKE_INPUT* trigger);
+
+
+
+		// Read a virtual key, together with modifiers, such as <^0x41
+		static BOOL ReadVirtualkey(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke);
+
+
+		// Expects a hexadecimal value
+		static BOOL ReadUnicode(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke);
+
+
+	public:
+
+		// This isn't an interpreter.
+		static BOOL ReadFile(std::ifstream* stream, std::vector<KEYBOARD> * ptrVectorKeyboard);
+	};
+
 	
 }
 
 
 
-void ParserTest();
-// LoadSettings will use this class (only one of its methods, actually
-// (making a static class instead of a namespace so that a bunch of its
-// methods can be private and out of the way)
-class Parser
-{
-private:
-	Parser() {}	// Nobody can make an instance of this.
 
-
-				// Reads alphabetic characters until a non-alphanumeric character is found
-				// The non-alpha is not read and not extracted
-				// Result is stored in the symbol parameter
-				// return value of FALSE means no symbol was found (end-of-file was found instead)
-	static BOOL ReadSymbol(std::ifstream* stream, std::string* symbol)
-	{
-		int input_char = 0;		// buffer for one character
-		symbol->clear();
-
-		if (stream->peek() == -1)			// If the very next thing is the end
-			return FALSE;					// then return false
-
-		while (isalpha(stream->peek()))			// if the next character is alphabetic
-		{
-			(*symbol) += (char)stream->get();		// then add that character to the string
-			if (stream->peek() == -1)				// if the next thing is an end-of-file,
-				return TRUE;						// then we're done
-		}											// and repeat
-
-													// reaching this point means we've encountered a non-alphabetic character
-		return TRUE;
-	}
-
-
-
-	// Read a keyboard name, such as \\?\ACPI#PNP0303#4&231b8599&0#{884b96c3-56ef-11d1-bc8c-00a0c91405dd}
-	// and store it in kb's device_name field
-	// Will only stop at whitespace (including newline) or a right parenthesis
-	static BOOL ReadKeyboardName(std::ifstream* stream, KEYBOARD* kb)
-	{
-		// from <codecvt>, the object that will convert between narrow and wide strings (C++11 and newer)
-		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-		// Because we read in UTF-8, but device names are stored internally as WCHARs.
-
-		auto buffer = std::string();					// utf-8
-		auto wideBuffer = std::wstring();				// utf-16
-
-		while (!isspace(stream->peek()) && stream->peek() != ')')
-		{
-			buffer += stream->get();
-		}
-		if (buffer == "") return FALSE;		// kb name wasn't there
-		wideBuffer = converter.from_bytes(buffer);			// convert and then store it in the wide buffer
-		swprintf_s(kb->device_name, kb->device_name_sizeof, L"%ls", wideBuffer.c_str());
-		return TRUE;
-	}
-
-
-	static BOOL ReadModifierHex(std::ifstream* stream, BYTE* modifiers, UINT32* code)
-	{
-		int input_char = 0;		// buffer for one character
-
-		int sideModifier = 0;	// 0: next modifier applies to left and right
-								// 1: left modifier
-								// 2: right modifier
-
-		(*modifiers) = 0;		// clearing...
-		(*code) = 0;
-
-		input_char = stream->peek();		// evaluate *next* character
-		while (true)
-		{
-			switch (input_char)					// TODO: Put this switch into a loop
-			{
-			case '<':					// next modifier is left only
-				sideModifier = 1;
-				stream->get();
-				break;
-
-			case '>':					// next modifier is right only
-				sideModifier = 2;
-				stream->get();
-				break;
-
-			case '#':					// Winkey (unaffected by side modifiers)
-				if (sideModifier == 1 || sideModifier == 0)
-					*modifiers |= MODIFIER_LWIN;
-				if (sideModifier == 2 || sideModifier == 0)
-					*modifiers |= MODIFIER_RWIN;		// (I've never seen a right Winkey)
-				sideModifier = 0;
-				stream->get();
-				break;
-
-			case '^':					// Ctrl
-				if (sideModifier == 1 || sideModifier == 0)
-					*modifiers |= MODIFIER_LCTRL;
-				if (sideModifier == 2 || sideModifier == 0)
-					*modifiers |= MODIFIER_RCTRL;
-				sideModifier = 0;
-				stream->get();
-				break;
-
-			case '!':					// Alt
-				if (sideModifier == 1 || sideModifier == 0)
-					*modifiers |= MODIFIER_LALT;
-				if (sideModifier == 2 || sideModifier == 0)
-					*modifiers |= MODIFIER_RALT;
-				sideModifier = 0;
-				stream->get();
-				break;
-
-			case '+':					// Shift
-				if (sideModifier == 1 || sideModifier == 0)
-					*modifiers |= MODIFIER_LSHIFT;
-				if (sideModifier == 2 || sideModifier == 0)
-					*modifiers |= MODIFIER_RSHIFT;
-				sideModifier = 0;
-				stream->get();
-				break;
-
-			case '0':
-			{	// brackets for locality
-				// expect an 'x' or 'X'
-				stream->get();
-				input_char = stream->peek();
-				if (input_char != 'x' && input_char != 'X')
-					return FALSE;
-				stream->get();
-				// while the next character is a hexadecimal digit, read it
-				std::string numberBuffer = std::string();
-				while (isxdigit(stream->peek()))
-				{
-					numberBuffer += stream->get();
-				}
-				if (sscanf_s(numberBuffer.c_str(), "%x", code) != 1)
-					return FALSE;		// that's one way to cast
-										// we're not trying to be fast, exactly
-										// scanf returns the amount of variables scanned
-				else return TRUE;
-			}
-
-			default:			// Do not extract character
-				return FALSE;
-			}	// end of switch
-		}	// loop
-	}
-
-
-	// Read an input keystroke, such as <+<!0x47
-	// (modifiers and a scancode)
-	// and place it into a trigger structure
-	// Returns FALSE upon encountering an unrecognized character
-	static BOOL ReadInputKeystroke(std::ifstream* stream, KEYSTROKE_INPUT* trigger)
-	{
-		UINT32 code = 0;
-		BOOL result = ReadModifierHex(stream, &(trigger->modifiers), &code);
-		trigger->scancode = (USHORT)code;			// lossy cast, but shouldn't lose any data
-		return result;
-	}
-
-
-
-	// Read a virtual key, together with modifiers, such as <^0x41
-	// Maybe we'll support multiple keypresses in the future, but not now.
-	static BOOL ReadVirtualkey(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke)
-	{
-		keystroke->flags = 0;
-		return ReadModifierHex(stream, &(keystroke->modifiers), &(keystroke->codepoint));
-	}
-
-
-
-	// Expects a hexadecimal value
-	static BOOL ReadUnicode(std::ifstream* stream, KEYSTROKE_OUTPUT* keystroke)
-	{
-		keystroke->flags = KEYEVENTF_UNICODE;
-
-		// All modifiers will be read but ignored
-		while (stream->peek() == '<'
-			|| stream->peek() == '>'
-			|| stream->peek() == '#'
-			|| stream->peek() == '^'
-			|| stream->peek() == '!'
-			|| stream->peek() == '+')
-			stream->get();
-
-		if (stream->peek() != '0')			// expect a hexadecimal
-			return FALSE;
-		stream->get();
-		if (stream->peek() != 'x' && stream->peek() != 'X')
-			return FALSE;
-		stream->get();
-
-		std::string numberBuffer = std::string();
-		while (isxdigit(stream->peek()))		// read all following hex digits
-		{
-			numberBuffer += stream->get();
-		}
-		if (sscanf_s(numberBuffer.c_str(), "%x", &(keystroke->codepoint)) != 1)
-			return FALSE;
-
-		return TRUE;
-
-	}
-
-
-public:
-
-	// This isn't an interpreter.
-	static BOOL ReadFile(std::ifstream* stream, std::vector<KEYBOARD> * ptrVectorKeyboard)
-	{
-		// having a function explicitly expect such a specific map is not very nice, but that's what I could do.
-
-		// Also, in the future we should consider supporting UTF-16 filenames. I don't know how to do that.
-
-		setlocale(LC_ALL, "");						// Set the locale, just in case
-
-
-		ptrVectorKeyboard->clear();					// clear all keyboards with the maps in them
-
-
-		// hold one line
-		auto lineBuffer = std::string();
-
-		// hold one symbol
-		auto symbol = std::string();
-
-		// hold the current keyboard being written to
-		auto keyboard = KEYBOARD();
-
-		// input and output
-		auto input = KEYSTROKE_INPUT();
-		auto output = KEYSTROKE_OUTPUT();
-
-		// one character
-		int read_char = 0;
-
-
-		// First term should be a "keyboard"
-		if (!ReadSymbol(stream, &symbol)) return FALSE;
-		if (symbol != "keyboard") return FALSE;
-		// expect left parenthesis
-		read_char = stream->get();
-		if (read_char != '(') return FALSE;
-		// read keyboard name
-		if (!ReadKeyboardName(stream, &keyboard)) return FALSE;
-		// expect right parenthesis
-		read_char = stream->get();
-		if (read_char != ')') return FALSE;
-		// jump to next line
-		getline(*stream, lineBuffer);
-		// begin loop
-		while (true)
-		{
-			if (!ReadSymbol(stream, &symbol))
-			{
-				// save keyboard and close
-				ptrVectorKeyboard->push_back(keyboard);
-				return TRUE;
-			}
-			// evaluate symbol
-			if (symbol == "unicode")
-			{
-				// case 1 - unicode
-				// expect a left parenthesis
-				read_char = stream->get();
-				if (read_char != '(') return FALSE;
-				// first parameter: an input keystroke
-				if (!ReadInputKeystroke(stream, &input)) return FALSE;
-				// expect a comma
-				read_char = stream->get();
-				if (read_char != ',') return FALSE;
-				// second parameter: an output keystroke with a unicode codepoint
-				if (!ReadUnicode(stream, &output)) return FALSE;
-				// expect a right parenthesis
-				read_char = stream->get();
-				if (read_char != ')') return FALSE;
-				// place input and output into current keyboard
-				keyboard.remaps.insert((std::pair<KEYSTROKE_INPUT, KEYSTROKE_OUTPUT>(input, output)));
-			}
-			else if (symbol == "virtual")
-			{
-				// case 2 - virtual key
-				// expect a left parenthesis
-				read_char = stream->get();
-				if (read_char != '(') return FALSE;
-				// first parameter: an input keystroke
-				if (!ReadInputKeystroke(stream, &input)) return FALSE;
-				// expect a comma
-				read_char = stream->get();
-				if (read_char != ')') return FALSE;
-				// second parameter: an output keystroke with a virtual key
-				if (!ReadVirtualkey(stream, &output)) return FALSE;
-				// expect a right parenthesis
-				read_char = stream->get();
-				if (read_char != ')') return FALSE;
-				// place input and output into current keyboard
-				keyboard.remaps.insert((std::pair<KEYSTROKE_INPUT, KEYSTROKE_OUTPUT>(input, output)));
-			}
-			else if (symbol == "keyboard")
-			{
-				// case 3 - new keyboard
-				// save this keyboard
-				ptrVectorKeyboard->push_back(keyboard);
-				// read a new one=
-				if (!ReadSymbol(stream, &symbol)) return FALSE;
-				if (symbol != "keyboard") return FALSE;
-				// expect left parenthesis
-				read_char = stream->get();
-				if (read_char != '(') return FALSE;
-				// read keyboard name
-				if (!ReadKeyboardName(stream, &keyboard)) return FALSE;
-				// expect right parenthesis
-				read_char = stream->get();
-				if (read_char != ')') return FALSE;
-				// jump to next line
-				getline(*stream, lineBuffer);
-			}
-			// jump line
-			getline(*stream, lineBuffer);
-
-		}	// loop
-	}
-};
